6ad9f1d200f884b328966a6489e0b26ab6a4c951
audun-web <audun@midtmeck.com>
2025-10-29 12:09:48 +0100

    stylet om oss siden med informasjonen innen for en boks, la til hover effekt på boksen

--- app.py
+++ app.py
@@ -2,20 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') #hjemside
 def index():
     return render_template('index.html')
 
-@app.route('/om_oss')
+@app.route('/om_oss') #om oss side
 def about():
     return render_template('om_oss.html')
 
-@app.route('/kontakt')
+@app.route('/kontakt') #kontakt side
 def contact():
     return render_template('kontakt.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) # debug slik at vi ikke må restarte siden for hver endring
 
 #
