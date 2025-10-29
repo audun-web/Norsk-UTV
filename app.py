1c5bb1786b83b1d391d37028c4f7daea0daae335
audun-web <audun@midtmeck.com>
2025-10-29 11:49:27 +0100

    la til route til de nye html filene

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/om_oss')
+def about():
+    return render_template('om_oss.html')
+
+@app.route('/kontakt')
+def contact():
+    return render_template('kontakt.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
