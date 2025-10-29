af268a6438b8f4447c3ab72b1877860b06d137bc
audun-web <audun@midtmeck.com>
2025-10-29 13:03:27 +0100

    endret kontaktinformasjonen og stylet kontakt listen med et grid med 4 bokser per rad

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ def about():
 @app.route('/kontakt') #kontakt side
 def kontact():
     kontaktliste = [
-        {"navn": "Kjartan Lang Holmen", "alder": "Hvem vet?", "fag": "God IT mann, er lat IT mann", "tlf": "+47 987 65 432"},
-        {"navn": "Ebbe Gassed On Zelow", "alder": "17", "fag": "230mg koffein om kvelden er bra :P", "tlf": "+47 245 23 678"}
-        {"navn": "Nikolai Haukland", "alder": "17", "fag": "Klarer ikke sitte vanlig", "tlf": "+47 234 56 789"}, 
-        {"navn": "Ludvig Haukland", "alder": "17", "fag": "Det er bare sånn det blir!", "tlf": "+47 123 45 678"}
+        {"navn": "Kjartan Lang Holmen", "alder": "Hvem vet?", "fag": "God IT mann, er lat IT mann", "tlf": "+47 987 65 432", "bilde": "Kjartan.jpeg"},
+        {"navn": "Ebbe Gassed On Zelow", "alder": "17", "fag": "230mg koffein om kvelden er bra :P", "tlf": "+47 245 23 678", "bilde": "ebbe.png"},
+        {"navn": "Nikolai Haukland", "alder": "17", "fag": "Klarer ikke sitte vanlig", "tlf": "+47 234 56 789", "bilde": "nikolai.png"}, 
+        {"navn": "Ludvig Haukland", "alder": "17", "fag": "Det er bare sånn det blir!", "tlf": "+47 123 45 678", "bilde": "ludvig.png"}
     ]
     return render_template('kontakt.html', kontaktliste=kontaktliste)
 
